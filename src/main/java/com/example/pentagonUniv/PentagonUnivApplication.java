8bdcb96d1b1cae139ae695efcad9819e53f3c609
junbean <60472117+junbean@users.noreply.github.com>
2025-11-18 12:41:01 +0900

    Initial commit

---
+++ src/main/java/com/example/pentagonUniv/PentagonUnivApplication.java
@@ -0,0 +1,13 @@
+package com.example.pentagonUniv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PentagonUnivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PentagonUnivApplication.class, args);
+	}
+
+}
